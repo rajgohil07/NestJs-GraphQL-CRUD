f6d8b16ba0728f0be0a463f6a5af2f300ccde5cf
rajgohil07 <raj.famous007@gmail.com>
2022-04-21 18:28:55 +0530

    changes in the book entiry

--- src/book/book.entity.ts
+++ src/book/book.entity.ts
@@ -1,13 +1,32 @@
 import { Field, Int, ObjectType } from '@nestjs/graphql';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
+@Entity()
 @ObjectType()
 export class BookEntity {
+  @PrimaryGeneratedColumn()
   @Field((type) => Int)
   ID: number;
 
+  @CreateDateColumn()
+  @Field((type) => Date)
+  CreatedDate: Date;
+
+  @UpdateDateColumn()
+  @Field((type) => Date)
+  UpdatedDate: Date;
+
+  @Column()
   @Field()
   Name: string;
 
+  @Column()
   @Field()
   Author: string;
 }
