57b3e10571b3f74aaf7b1391d91a5712855908ff
rajgohil07 <raj.famous007@gmail.com>
2022-04-21 15:54:56 +0530

    added module and services

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostgresDataSource } from './config/database-config';
+import { BookModule } from './book/book.module';
+import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
-  // imports: [TypeOrmModule.forRoot(PostgresDataSource)],
+  imports: [
+    TypeOrmModule.forRoot(PostgresDataSource),
+    GraphQLModule.forRoot(),
+    BookModule,
+  ],
   controllers: [],
   providers: [],
 })
---
+++ src/book/book.module.ts
@@ -0,0 +1,7 @@
+import { Module } from '@nestjs/common';
+import { BookService } from './book.service';
+
+@Module({
+  providers: [BookService]
+})
+export class BookModule {}
