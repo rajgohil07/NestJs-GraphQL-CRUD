6db6544200b676f29de88a0b5eba8983c8b51953
rajgohil07 <raj.famous007@gmail.com>
2022-04-22 11:23:46 +0530

    added logging option to true

--- src/config/database-config.ts
+++ src/config/database-config.ts
@@ -12,4 +12,5 @@ export const PostgresDataSource: PostgresConnectionOptions = {
   type: 'postgres',
   entities: [BookEntity],
   synchronize: true,
+  logging: ['query', 'error', 'schema', 'migration', 'warn'],
 };
